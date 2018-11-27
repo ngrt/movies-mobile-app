f3f7b41623218104807a5b7e26f1221a453d76f6
Noufel Gouirhate <noufel.gouirhate@gmail.com>
2018-11-27 12:54:11 +0100

    setting-up redux

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import thunk from "redux-thunk";
+import {composeWithDevTools} from 'remote-redux-devtools';
+import moviesReducer from "./store/reducers/movies";
+import {Provider} from "react-redux";
+
+const rootReducer = combineReducers({
+    movies: moviesReducer,
+});
+
+
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Text>Noufel is killing it</Text>
+        </View>
+      </Provider>
     );
   }
 }
